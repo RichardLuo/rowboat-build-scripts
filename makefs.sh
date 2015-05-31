ff87ae5bb8eacf967de6d0ba17be38f083a2871e
rxwen <rx.wen218@gmail.com>
2015-05-31 22:47:52 +0800

    chmod .prop files to make sure they will be loaded by init on android

--- makefs.sh
+++ makefs.sh
@@ -7,6 +7,9 @@
 
 rm -rf android_rootfs
 mkdir android_rootfs
+# the default.prop's chmod must be 644, otherwise, it won't be loaded by init
+chmod 644 root/default.prop
 cp -r root/* android_rootfs
 cp -r system android_rootfs
+chmod 644 android_rootfs/system/build.prop
 ../../../../build/tools/mktarball.sh ../../../host/linux-x86/bin/fs_get_stats android_rootfs . rootfs rootfs.tar.bz2
