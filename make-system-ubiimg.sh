a9cf660cd974c690fee50caadb918757d7f5c343
rxwen <rx.wen218@gmail.com>
2017-02-08 01:17:09 +0800

    update ubifs generation script

--- make-system-ubiimg.sh
+++ make-system-ubiimg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkfs.ubifs -q -r system -m 2048 -e 126976 -c 384 -o system-ubifs.img 
+mkfs.ubifs -q -r system -m 2048 -e 126976 -c 512 -o system-ubifs.img
 ubinize -o system-ubi.img -m 2048 -p 128KiB ../../../../scripts/ubinize-system.cfg
